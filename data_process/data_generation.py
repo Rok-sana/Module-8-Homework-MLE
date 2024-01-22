20d606e0010179d634b4c4482298f92125b80551
Oksana Bolibok <oksanabolibok496@gmail.com>
2024-01-22 21:35:49 +0400

    Added requirements. Added data generation layout, code refactoring

--- data_process/data_generation.py
+++ data_process/data_generation.py
@@ -1,10 +1,15 @@
 # Importing required libraries
-import numpy as np
-import pandas as pd
 import logging
 import os
 import sys
 import json
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+import numpy as np
+
+# Static variables
+RANDOM_STATE = 42
 
 # Create logger
 logger = logging.getLogger()
@@ -19,9 +24,8 @@ DATA_DIR = os.path.abspath(os.path.join(ROOT_DIR, '../data'))
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
 
-# Change to CONF_FILE = "settings.json" if you have problems with env variables
-CONF_FILE = os.getenv('CONF_PATH')
-
+# Change to CONF_FILE = os.getenv('CONF_PATH') if you have problems with env variables
+CONF_FILE = "settings.json"
 # Load configuration settings from JSON
 logger.info("Loading configuration settings from JSON...")
 with open(CONF_FILE, "r") as file:
@@ -33,45 +37,42 @@ DATA_DIR = get_project_dir(conf['general']['data_dir'])
 TRAIN_PATH = os.path.join(DATA_DIR, conf['train']['table_name'])
 INFERENCE_PATH = os.path.join(DATA_DIR, conf['inference']['inp_table_name'])
 
-# Singleton class for generating XOR data set
+# Singleton class for generating Iris dataset
 @singleton
-class XorSetGenerator():
+class IrisDatasetGenerator():
     def __init__(self):
-        self.df = None
-
-    # Method to create the XOR data
-    def create(self, len: int, save_path: os.path, is_labeled: bool = True):
-        logger.info("Creating XOR dataset...")
-        self.df = self._generate_features(len)
-        if is_labeled:
-            self.df = self._generate_target(self.df)
-        if save_path:
-            self.save(self.df, save_path)
-        return self.df
-
-    # Method to generate features
-    def _generate_features(self, n: int) -> pd.DataFrame:
-        logger.info("Generating features...")
-        x1 = np.random.choice([True, False], size=n)
-        x2 = np.random.choice([True, False], size=n)
-        return pd.DataFrame(list(zip(x1, x2)), columns=['x1', 'x2'])
-
-    # Method to generate target
-    def _generate_target(self, df: pd.DataFrame) -> pd.DataFrame:
-        logger.info("Generating target...")
-        df['y'] = np.logical_xor(df['x1'], df['x2'])
-        return df
+        self.df_train = None
+        self.df_inference = None
+        
+    def load_data(self):
+        # Load iris dataset from scikit-learn library
+        iris = load_iris()
+        data = np.c_[iris.data, iris.target]
+        columns = np.append(iris.feature_names, ["target"])
+        iris_full_df = pd.DataFrame(data, columns=columns)
+        logger.info(f"Loading iris data ...")   
+        return iris_full_df
     
-    # Method to save data
-    def save(self, df: pd.DataFrame, out_path: os.path):
-        logger.info(f"Saving data to {out_path}...")
-        df.to_csv(out_path, index=False)
+    def split_data(self, iris_full_df, test_size=0.2, random_state=RANDOM_STATE):
+        # Split the data into train and test sets
+        train_data, inference_data = train_test_split(iris_full_df, test_size=test_size, random_state=random_state)
+        self.df_train = train_data
+        self.df_inference = inference_data.drop(columns=['target'])
+        logger.info(f"Splitting iris data ...")   
+
+    def save_datasets(self, train_filepath, inference_filepath):
+        # Save train and inference datasets to CSV files
+        self.df_train.to_csv(train_filepath, index=False)
+        logger.info(f"Saving train data to {train_filepath}...")
+        self.df_inference.to_csv(INFERENCE_PATH, index=False)
+        logger.info(f"Saving inference data to {inference_filepath}...")    
 
 # Main execution
 if __name__ == "__main__":
     configure_logging()
     logger.info("Starting script...")
-    gen = XorSetGenerator()
-    gen.create(len=256, save_path=TRAIN_PATH)
-    gen.create(len=64, save_path=INFERENCE_PATH, is_labeled=False)
+    iris_dataset_generator = IrisDatasetGenerator()
+    iris_full_df = iris_dataset_generator.load_data()
+    iris_dataset_generator.split_data(iris_full_df, test_size=0.2, random_state=42)
+    iris_dataset_generator.save_datasets(TRAIN_PATH, INFERENCE_PATH)
     logger.info("Script completed successfully.")
